02c2945756a7108150548eceb3c07e44ac8399ac
george <gemarquesf@gmail.com>
2025-05-10 12:25:50 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/georgecmf.png" />
-    <Titulo fontSize={20}>George Marques</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>George Marques</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        georgecmf
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor júnior front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
